f1b24b758ebfdd399a97b9fe6c410755790123ee
Goonnya <95735892+Goonnya@users.noreply.github.com>
2021-12-08 10:41:53 +0400

    Добавьте файлы проекта.

---
+++ Builder/House.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    // Состав стены
+    class Wall
+    {
+        // Из чего строится дом
+        public string Type { get; set; }
+    }
+    // Цемент
+    class Cement
+    { }
+    // Стройительные материалы
+    class BuildingMaterials
+    {
+        public string Name { get; set; }
+    }
+    // Стекло
+    class Glass
+    { }
+
+    class House
+    {
+        // Стена
+        public Wall Wall { get; set; }
+        // Цемент
+        public Cement Cement { get; set; }
+        // Строительные материалы
+        public BuildingMaterials BuildingMaterials { get; set; }
+        // Стекло
+        public Glass Glass { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Wall != null)
+                sb.Append(Wall.Type + "\n");
+            if (Cement != null)
+                sb.Append("Цемент \n");
+            if (Glass != null)
+                sb.Append("Стекло \n");
+            if (BuildingMaterials != null)
+                sb.Append("Строительные материалы: " + BuildingMaterials.Name + " \n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем объект архитектор
+            Architect architect = new Architect();
+            // создаем билдер для деревянного дома
+            HouseBuilder builder = new WoodHouseBuilder();
+            // проектируем
+            House woodHouse = architect.Design(builder);
+            Console.WriteLine(woodHouse.ToString());
+            // создаем билдер для кирпичного дома
+            builder = new BrickHouseBuilder();
+            // проектируем
+            House brickHouse = architect.Design(builder);
+            Console.WriteLine(brickHouse.ToString());
+
+            Console.Read();
+        }
+    }
+    // абстрактный класс строителя
+    abstract class HouseBuilder
+    {
+        public House House { get; private set; }
+        public void CreateHouse()
+        {
+            House = new House();
+        }
+        public abstract void SetWall();
+        public abstract void SetCement();
+        public abstract void SetBuildingMaterials();
+        public abstract void SetGlass();
+    }
+    // Архитектор
+    class Architect
+    {
+        public House Design(HouseBuilder houseBuilder)
+        {
+            houseBuilder.CreateHouse();
+            houseBuilder.SetWall();
+            houseBuilder.SetCement();
+            houseBuilder.SetBuildingMaterials();
+            houseBuilder.SetGlass();
+            return houseBuilder.House;
+        }
+    }
+    // строитель для деревянного дома
+    class WoodHouseBuilder : HouseBuilder
+    {
+        public override void SetWall()
+        {
+            this.House.Wall = new Wall { Type = "Брёвна для стен" };
+        }
+        public override void SetGlass()
+        {
+            this.House.Glass = new Glass();
+        }
+
+        public override void SetCement()
+        {
+            this.House.Cement = new Cement();
+        }
+
+        public override void SetBuildingMaterials()
+        {
+            this.House.BuildingMaterials = new BuildingMaterials { Name = "Лак для дерева, гвозди" };
+        }
+    }
+    // строитель для кирпичного дома
+    class BrickHouseBuilder : HouseBuilder
+    {
+        public override void SetWall()
+        {
+            this.House.Wall = new Wall { Type = "Кирпичи" };
+        }
+
+        public override void SetCement()
+        {
+            this.House.Cement = new Cement();
+        }
+        public override void SetGlass()
+        {
+            this.House.Glass = new Glass();
+        }
+
+        public override void SetBuildingMaterials()
+        {
+            this.House.BuildingMaterials = new BuildingMaterials { Name = "Краска, штукатурка, обои" };
+        }
+    }
+}
